c0dd0205514ee8786bd15552dbd8b2688b7a63a9
peter <ptmartinezsound@gmail.com>
2021-04-28 11:12:40 -0500

    update

--- server.js
+++ server.js
@@ -1,5 +1,8 @@
 const express = require('express');
 const app = express();
+const { notes } = require('./Develop/db/db.json');
+
+app.get('./')
 
 app.listen(3001, () => {
     console.log(`API server now on port 3001!`);
