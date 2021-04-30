7012dceaa47cf0f53fba566e9156e8a1bf6a99b4
peter <ptmartinezsound@gmail.com>
2021-04-30 16:29:20 -0500

    fixing api routes

--- server.js
+++ server.js
@@ -2,7 +2,15 @@ const express = require('express');
 const app = express();
 const { notes } = require('./Develop/db/db.json');
 
-app.get('./')
+const apiRoutes = require('./lib/apiRoutes');
+const htmlRoutes = require('./lib/htmlRoutes');
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(express.static('Develop/public'));
+
+app.use('/api', apiRoutes);
+app.use('/', htmlRoutes);
 
 app.listen(3001, () => {
     console.log(`API server now on port 3001!`);
